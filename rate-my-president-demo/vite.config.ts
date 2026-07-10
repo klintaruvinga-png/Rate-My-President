44be1b6e5cb403fb0b31f3941de75b37d89c54e6
KudzieUN <help@stokvelsociety.co.za>
2026-07-10 15:45:14 +0200

    Update vite.config.ts

--- rate-my-president-demo/vite.config.ts
+++ rate-my-president-demo/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: path.resolve(__dirname, '../public'),
   resolve: {
     alias: [
       { find: '@root', replacement: path.resolve(__dirname, '..') },
